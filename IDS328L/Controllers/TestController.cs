90a0e104a67bdfec4b0fa91723f1acbbaf19533b
VToribiot <vtoribiot@gmail.com>
2023-04-04 09:28:31 -0400

    Testing the push event doing a stupid commit

---
+++ IDS328L/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using IDS328L.DTO;
+using IDS328L.Services;
+
+namespace CORE_Api_Pymes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
